21583c7147328665c828dde10f776b4a5b206894
bulanndhln <bullandhln@gmail.com>
2023-05-28 18:18:59 +0700

    login, register, updateprofile, getuser done

--- route/index.js
+++ route/index.js
@@ -4,7 +4,7 @@ const verifiToken = require('../middleware/verifikasi.js')
 const userController = require('../controller/User.js')
 const verif = require('../middleware/verifikasi.js')
 const version = '/api/v1'
-//user
+    //user
 router.get(`${version}/users`, userController.users)
 router.post(`${version}/register`, userController.register)
 router.post(`${version}/login`, userController.login)
